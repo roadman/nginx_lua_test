48f21cacd6eb90d17d0a7ebf37be6124c45bbf26
seiji ochiai <roadmang@gmail.com>
2014-04-01 18:46:49 +0900

    add tools script

---
+++ tools/check_request.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+RES_OK="source[1234567890abcdefghijklmnopqrstuvwxyz], encode[313233343536373839306162636465666768696a6b6c6d6e6f707172737475767778797a], decode[1234567890abcdefghijklmnopqrstuvwxyz], result[OK]"
+RES=`curl -s "http://localhost/hexdigest/"`
+
+if [ "$RES_OK" = "$RES" ]; then
+    echo "check ok!! => $RES"
+else
+    echo "check ng!! => $RES"
+fi
