24e27bb4e244e4a6f34178e6e3ee09ca0d3e9e11
seiji ochiai <roadmang@gmail.com>
2014-04-01 18:33:12 +0900

    add site-cookbook nginx_lua_test init

---
+++ site-cookbooks/nginx_lua_test/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: nginx_test
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{git lua-devel pcre-devel valgrind curl}.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+service 'iptables' do
+    action [:disable, :stop]
+end
