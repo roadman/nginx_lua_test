bf81f486c60ac2cc5ecf75ff97878f151786fffc
seiji ochiai <roadmang@gmail.com>
2014-04-02 12:27:50 +0900

    bugfix: seup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ bundle install --path=vendor/bundle
 git submodule init
 git submodule update
 vagrant up
-vagrant ssh-config nginx_lua_test >> ~/.ssh/config
+vagrant ssh-config --host nginx_lua_test >> ~/.ssh/config
 bundle exec knife solo prepare nginx_lua_test
 bundle exec knife solo cook nginx_lua_test
 
